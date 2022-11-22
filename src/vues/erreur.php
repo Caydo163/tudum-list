8939919fbb55a89d83ce60f371c46bcfaddf36d1
Evan FORISSIER <Evan.FORISSIER@etu.uca.fr>
2022-11-22 10:00:05 +0100

    MAJ Evan Vue Erreur debut gestion erreur

---
+++ src/vues/erreur.php
@@ -0,0 +1,36 @@
+
+<html>
+    <head>
+        <title>TUDUM-LIST</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+        <link href="styleErreur.css" rel="stylesheet">
+    </head>
+    <main>
+        <header>
+        </header>
+        
+        <body>
+            
+            <div class="text-bg-dark">
+                <h1>TUDUM-LIST</h1>   
+                <h4>Erreur</h4>
+                <p>
+                    Coucou je suis un message d'erreur personnalisé (mais je suis un peu con je marche pas encore)
+                </p>
+                <?php
+
+                    foreach ($TabErreur as $value){
+                        $word .="'$value',"; 
+                    }
+                    echo "$word";
+
+                ?>
+                
+                </div>
+            </div>
+        </body>
+        <footer>
+                
+        </footer>
+    </main>
+</html>
---
+++ src/vues/fichiertemporaire.php
@@ -0,0 +1,5 @@
+<html>
+    <body>
+        <h7> Coucou je vais disparaitre quand on va gérer les erreurs</h7>
+        </body
+    </html>
