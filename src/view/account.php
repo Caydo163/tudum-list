19624546d95353abc8bcd09575636ddc250e1b8e
Justin CARVALHEIRO <Justin.carvalheiro16@gmail.com>
2022-12-03 13:51:43 +0100

    popup confirmation suppression

--- src/view/account.php
+++ src/view/account.php
@@ -5,6 +5,8 @@
         <meta charset="utf-8" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
+        <!-- JavaScript Bundle with Popper -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
         <link href="view/style.css" rel="stylesheet">
     </head>
     <main>
@@ -16,8 +18,28 @@
 
                     <?php if(isset($_SESSION['role']) && $_SESSION['role'] == 'user'): ?>
                         <h3 style="color:white">Connecté en tant que <?= $_SESSION['login'] ?></h3>
-                        <a type="submit" class="btn btn-warning mt-3" style="background-color:#E50914;border:#E50914 1px solid;" href="?action=u-deconnexion">Se déconnecter</a>
-                        <a type="submit" class="btn btn-outline-light mt-3" href="?action=u-delete_account">Supprimer compte</a>
+                        <a class="btn btn-warning mt-3" style="background-color:#E50914;border:#E50914 1px solid;" href="?action=u-deconnexion">Se déconnecter</a>
+                        
+                        <!-- <a type="submit" class="btn btn-outline-light mt-3" href="?action=u-delete_account">Supprimer compte</a> -->
+                        <button type="button" class="btn btn-outline-light mt-3" data-bs-toggle="modal" data-bs-target="#delete_popup">Supprimer compte</button>
+
+                        <div class="modal fade" id="delete_popup" aria-labelledby="delete_popupLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h1 class="modal-title fs-5" id="exampleModalLabel">Confirmation suppression</h1>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        Voulez-vous vraiment supprimer votre compte ?
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annuler</button>
+                                        <a type="button" class="btn btn-primary" href="?action=u-delete_account">Supprimer</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     
                     <?php else: ?>
                         <form method="POST">
